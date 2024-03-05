51b12558385725620643b2ad0d54d07e0a1016bd
Srdan Nikolic <xanatos1337@gmail.com>
2024-03-05 08:13:42 +0100

    renamed version cookie

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div v-if="sessionCheckDone" class="app-container">
-    <NevsLoader v-if="$store.state.loaderCount > 0"></NevsLoader>
-    <NevsNotification></NevsNotification>
-    <NevsPopup></NevsPopup>
-    <LoginForm :postLogin="setupMenu" v-if="$store.state.user === null"></LoginForm>
-    <template v-if="$store.state.user !== null">
-      <Transition name="main-menu">
-        <NevsMainMenu v-show="showMenu" :items="menu.items" :logo="menu.logo"
-                      @toggleMenu="showMenu=!showMenu"></NevsMainMenu>
-      </Transition>
-      <NevsTopBar :breadcrumbs="$store.state.breadcrumbs" :buttons="topBarButtons"
-                  @toggleMenu="showMenu=!showMenu"></NevsTopBar>
-      <div class="nevs-main-content">
-        <RouterView></RouterView>
-      </div>
-    </template>
-  </div>
+    <div v-if="sessionCheckDone" class="app-container">
+        <NevsLoader v-if="$store.state.loaderCount > 0"></NevsLoader>
+        <NevsNotification></NevsNotification>
+        <NevsPopup></NevsPopup>
+        <LoginForm :postLogin="setupMenu" v-if="$store.state.user === null"></LoginForm>
+        <template v-if="$store.state.user !== null">
+            <Transition name="main-menu">
+                <NevsMainMenu v-show="showMenu" :items="menu.items" :logo="menu.logo"
+                              @toggleMenu="showMenu=!showMenu"></NevsMainMenu>
+            </Transition>
+            <NevsTopBar :breadcrumbs="$store.state.breadcrumbs" :buttons="topBarButtons"
+                        @toggleMenu="showMenu=!showMenu"></NevsTopBar>
+            <div class="nevs-main-content">
+                <RouterView></RouterView>
+            </div>
+        </template>
+    </div>
 </template>
 
 <script>
@@ -28,114 +28,117 @@ import NevsMainMenu from "@/components/nevs/NevsMainMenu";
 import NevsTopBar from "@/components/nevs/NevsTopBar";
 
 export default {
-  name: 'App',
-  components: {
-    NevsMainMenu,
-    NevsTopBar,
-    LoginForm,
-    NevsPopup,
-    NevsNotification,
-    NevsLoader
-  },
-  data() {
-    return {
-      showMenu: true,
-      sessionCheckDone: false,
-      menu: {
-        logo: 'https://via.placeholder.com/250x100',
-        items: []
-      },
-      topBarButtons: [
-        {
-          icon: '<i class="fa-solid fa-right-from-bracket"></i>',
-          tooltip: this.$LANG.Get('tooltips.logout'),
-          action: () => {
-            let vm = this;
-            this.$API.APICall('post', 'logout', {}, (data, success) => {
-              if (success) {
-                vm.$store.commit('setUser', null);
-              } else {
-                vm.$LOCAL_BUS.TriggerEvent('popup', {text: vm.$LANG.Get('alerts.serverError'), type: 'alert'});
-              }
-            });
-          }
-        },
-        {
-          icon: '<i class="fa-solid fa-user"></i>',
-          tooltip: this.$LANG.Get('tooltips.myProfile'),
-          action: () => {
-            this.$router.push('/users/' + this.$store.state.user.id);
-          }
-        }
-      ]
-    }
-  },
-  methods: {
-    resolveWindowResize() {
-      this.showMenu = window.innerWidth >= 800;
+    name: 'App',
+    components: {
+        NevsMainMenu,
+        NevsTopBar,
+        LoginForm,
+        NevsPopup,
+        NevsNotification,
+        NevsLoader
     },
-    setupMenu() {
-      this.menu.items = [];
-      this.menu.items.push({
-        id: 'home',
-        label: this.$LANG.Get('modules.home'),
-        link: '/',
-        icon: '<i class="fa-solid fa-house"></i>',
-        children: []
-      });
-      if(this.$store.state.user.permissions.includes('MANAGE_USERS')) {
-        this.menu.items.push({
-          id: 'users',
-          label: this.$LANG.Get('modules.users'),
-          link: '/users',
-          icon: '<i class="fa-solid fa-users"></i>',
-          children: []
-        });
-      }
-    }
-  },
-  mounted() {
-    window.addEventListener('resize', this.resolveWindowResize);
-    this.resolveWindowResize();
-    let vm = this;
-    this.$API.APICall('get', 'public/version', {}, (data, success) => {
-      if (success) {
-        if (data.version !== this.$HELPERS.GetCookie('pausalko_version')) {
-          this.$HELPERS.SetCookie('nevs_version', data.version);
-          window.location.reload(true);
+    data() {
+        return {
+            showMenu: true,
+            sessionCheckDone: false,
+            menu: {
+                logo: 'https://via.placeholder.com/250x100',
+                items: []
+            },
+            topBarButtons: [
+                {
+                    icon: '<i class="fa-solid fa-right-from-bracket"></i>',
+                    tooltip: this.$LANG.Get('tooltips.logout'),
+                    action: () => {
+                        let vm = this;
+                        this.$API.APICall('post', 'logout', {}, (data, success) => {
+                            if (success) {
+                                vm.$store.commit('setUser', null);
+                            } else {
+                                vm.$LOCAL_BUS.TriggerEvent('popup', {
+                                    text: vm.$LANG.Get('alerts.serverError'),
+                                    type: 'alert'
+                                });
+                            }
+                        });
+                    }
+                },
+                {
+                    icon: '<i class="fa-solid fa-user"></i>',
+                    tooltip: this.$LANG.Get('tooltips.myProfile'),
+                    action: () => {
+                        this.$router.push('/users/' + this.$store.state.user.id);
+                    }
+                }
+            ]
         }
-      }
-    });
-    this.$API.APICall('get', 'session', {}, (data, success) => {
-      if (success) {
-        vm.$store.commit('setUser', data.user);
-        vm.$store.commit('setLocale', data.user.locale);
-        vm.$nextTick(() => {
-          this.setupMenu();
+    },
+    methods: {
+        resolveWindowResize() {
+            this.showMenu = window.innerWidth >= 800;
+        },
+        setupMenu() {
+            this.menu.items = [];
+            this.menu.items.push({
+                id: 'home',
+                label: this.$LANG.Get('modules.home'),
+                link: '/',
+                icon: '<i class="fa-solid fa-house"></i>',
+                children: []
+            });
+            if (this.$store.state.user.permissions.includes('MANAGE_USERS')) {
+                this.menu.items.push({
+                    id: 'users',
+                    label: this.$LANG.Get('modules.users'),
+                    link: '/users',
+                    icon: '<i class="fa-solid fa-users"></i>',
+                    children: []
+                });
+            }
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.resolveWindowResize);
+        this.resolveWindowResize();
+        let vm = this;
+        this.$API.APICall('get', 'public/version', {}, (data, success) => {
+            if (success) {
+                if (data.version !== this.$HELPERS.GetCookie('nevs_version')) {
+                    this.$HELPERS.SetCookie('nevs_version', data.version);
+                    window.location.reload(true);
+                }
+            }
         });
-      } else {
-        vm.$store.commit('setLocale', vm.$store.state.settings.LOCALE);
-      }
-      vm.$nextTick(() => {
-        vm.sessionCheckDone = true;
-      });
-    }, false);
-    setInterval(() => {
-      if (vm.$store.state.user !== null) {
-        this.$API.APICall('get', 'public/heartbeat', {}, (data, success) => {
-          if (success) {
-            if (!data.logged_in) {
-              vm.$store.commit('setUser', null);
+        this.$API.APICall('get', 'session', {}, (data, success) => {
+            if (success) {
+                vm.$store.commit('setUser', data.user);
+                vm.$store.commit('setLocale', data.user.locale);
+                vm.$nextTick(() => {
+                    this.setupMenu();
+                });
+            } else {
+                vm.$store.commit('setLocale', vm.$store.state.settings.LOCALE);
             }
-            if (data.version !== this.$HELPERS.GetCookie('pausalko_version')) {
-              this.$HELPERS.SetCookie('nevs_version', data.version);
-              window.location.reload(true);
-            }
-          }
+            vm.$nextTick(() => {
+                vm.sessionCheckDone = true;
+            });
         }, false);
-      }
-    }, 30000);
-  }
+        setInterval(() => {
+            if (vm.$store.state.user !== null) {
+                this.$API.APICall('get', 'public/heartbeat', {}, (data, success) => {
+                    if (success) {
+                        if (!data.logged_in) {
+                            vm.$store.commit('setUser', null);
+                        }
+                        if (data.version !== this.$HELPERS.GetCookie('nevs_version')) {
+                            this.$HELPERS.SetCookie('nevs_version', data.version);
+                            window.location.reload(true);
+                        }
+                    }
+                }, false);
+            }
+        }, 30000);
+    }
 }
 
 </script>
